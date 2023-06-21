f675cb121223b9f902cb3922031622fa472a5531
Issam Mezrani <kaiss94@gmail.com>
2023-06-21 21:14:09 +0200

    ras

--- tests/test_sauce.py
+++ tests/test_sauce.py
@@ -13,7 +13,7 @@ class TestSauce(TestCase):
         pizza_name = tuple(RECIPES.keys())[-1]
         old = fridge.total_sauce_unit
         status, message = maker.take_an_order(pizza_name)
-        self.assertEqual(old, fridge.total_sauce_unit + 1)
+        self.assertEqual(old, fridge.total_sauce_unit +1)
         self.assertTrue(status)
         self.assertIsNone(message)
         fridge.empty_sauce_jars()
