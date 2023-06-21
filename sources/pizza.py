a86a865c8cdcfd8f7b2ec2556d7618fecdd06a01
Issam Mezrani <kaiss94@gmail.com>
2023-06-21 19:10:26 +0200

    ajout pizza djerbienne

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'djerbienne': {
+        'ingredients': {'mozzarella': 3, 'pepper': 5, 'onion': 5, 'ham': 4},
+        'price': 12,
     }
 }
